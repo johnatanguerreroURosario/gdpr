dd41f2a106278da42cefd7ab19ddffa225e8a2f1
Johnatan Rodolfo Guerrero Guerrero <johnatan.guerrero@urosario.edu.co>
2024-11-15 10:39:22 -0500

    agregar menu accesibilidad

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'GTM-K7DQ2NRJ'); // Reemplaza con tu ID de Google Analytics
 
 //Test if gtag is working
+/*
 document.getElementById('ga-evento-local').addEventListener('click', function() {
-    console.log('Evento local');
-    window.gtag('event', 'interaccion_boton_accesibilidad', {
-        'event_category': 'Accesibilidad',
-        'event_label': 'Botón 1 presionado',
-        'value': 1
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'abrir_widget_accesibilidad',
+        event_category: 'Accesibilidad',
+        event_label: 'Widget abierto',
+        value: 1
     });
 });
 
@@ -19,3 +17,6 @@ window.onerror = (errorMsg, url, lineNumber) => {
     });
     console.error(errorMsg, url, lineNumber);
 };
+
+*/
+
